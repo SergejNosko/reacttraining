9be1f213c3779a8284c253a0fc785a1cf30db6d3
SergejNosko <tuzemec81@gmail.com>
2017-06-13 11:02:11 +0300

    add an api.js file, where we make a request to github api. Then we parse it and show in our view.

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,38 +1,98 @@
-const React = require('react');
+const React = require('react'),
+      PropTypes = require('prop-types'),
+      api = require('../utils/api');
+
+function RepoGrid(props) {
+    return(
+        <ul className="popular-list">
+            {props.repos.map((repo, index) => {
+                return(
+                    <li key={repo.name} className="popular-item">
+                        <div className="popular-rank">#{index + 1}</div>
+                        <ul className="space-list-items">
+                            <li>
+                                <img
+                                    className="avatar"
+                                    src={repo.owner.avatar_url}
+                                    alt={'Avatar for ' + repo.owner.login}
+                                />
+                            </li>
+                            <li><a href={repo.html_url}>{repo.name}</a></li>
+                            <li>@{repo.owner.login}</li>
+                            <li>{repo.stargazers_count} stars</li>
+                        </ul>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+};
+
+function SelectLanguage(props) {
+    let languages = ['All', 'JavaScript', 'Ruby', 'Java', 'Css', 'Python'];
+    return(
+        <ul className="languages">
+            {languages.map((lang) =>{/*if we use an arrow func the context is the same as the context outside*/
+                return (
+                    <li
+                        key={lang}
+                        onClick={props.onSelect.bind(null, lang)}
+                        style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}>
+                        {lang}
+                    </li>
+                )
+            }/*specific contex*/)}
+        </ul>
+    )
+}
+
+SelectLanguage.propTypes = {
+    selectedLanguage: PropTypes.string.isRequired,
+    onSelect: PropTypes.func.isRequired
+};
 
 class Popular extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            selectedLanguage: 'All'
+            selectedLanguage: 'All',
+            repos: null
         };
         this.updateLanguage = this.updateLanguage.bind(this);
+    }
+
+    componentDidMount(){
+        this.updateLanguage(this.state.selectedLanguage);
     }
 
     updateLanguage(lang){
         this.setState(function () {
            return {
-               selectedLanguage: lang
+               selectedLanguage: lang,
+               repos: null
            };
         });
+        api.fetchPopularRepos(lang).then(function (repos) {
+                this.setState(function () {
+                    return {
+                        repos: repos
+                    }
+                });
+            }.bind(this));
     }
 
     render(){
-        let languages = ['All', 'JavaScript', 'Ruby', 'Java', 'Css', 'Python'];
-
         return (
-            <ul className="languages">
-                {languages.map((lang) =>{/*if we use an arrow func the context is the same as the context outside*/
-                    return (
-                        <li
-                            key={lang}
-                            onClick={this.updateLanguage.bind(null, lang)}
-                            style={lang === this.state.selectedLanguage ? {color: '#d0021b'} : null}>
-                                {lang}
-                        </li>
-                    )
-                }/*specific contex*/)}
-            </ul>
+            <div>
+                <SelectLanguage selectedLanguage={this.state.selectedLanguage} onSelect={this.updateLanguage}/>
+                {!this.state.repos
+                ? 'LOADING'
+                : <RepoGrid repos={this.state.repos} />}
+            </div>
         )
     }
 }
