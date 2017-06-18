58234f24b6f4c95d59a8e43aaf3b2f292a630cd3
SergejNosko <tuzemec81@gmail.com>
2017-06-18 10:51:14 +0300

    add several functions such as getProfile, getRepos, getStarCount, calculateScore, handleError, getUserData, sortPlayers, composed them all and add them to module.exports as a battle function

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,64 @@
 const axios = require('axios');
 
+let id = 'MY_CLIENT_ID',
+    secret = 'MY_CLIENT_SECRET',
+    params = '?client_id=' + id + '&client_secret=' + secret;
+
+function getProfile(username) {
+    return axios.get('https://api.github.com/users/' + username + params)
+        .then((user) => {
+            return user.data;
+        });
+}
+
+function getRepos(username) {
+    return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
+}
+
+function getStarCount(repos) {
+    return repos.data.reduce((count, repo) => {
+        return count + repo.stargazers_count;
+    }, 0);
+}
+
+function calculateScore(profile, repos) {
+    let followers = profile.followers,
+        totalStars = getStarCount(repos);
+
+    return (followers * 3) + totalStars;
+}
+
+function handleError(error) {
+    console.warn(error);
+    return null;
+}
+
+function getUserData(player) {
+    return axios.all([
+        getProfile(player),
+        getRepos(player),
+    ]).then(function (data) {
+        let profile = data[0],
+            repos = data[1];
+        return {
+            profile: profile,
+            score: calculateScore(profile, repos)
+        }
+    });
+}
+
+function sortPlayers(players) {
+    return players.sort(function (a, b) {
+       return b.score - a.score;
+    });
+}
+
 module.exports = {
+    battle: function (players) {
+        return axios.all(players.map(getUserData))
+            .then(sortPlayers)
+            .catch(handleError);
+    },
     fetchPopularRepos: (language) => {
         let encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
 
