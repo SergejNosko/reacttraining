fca573494134d410ef7e8b1fd041fb187735d633
SergejNosko <tuzemec81@gmail.com>
2017-06-19 11:59:06 +0300

    created separate file for PlayerPreview component, finished Results component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,33 +1,7 @@
 const React = require('react'),
     PropTypes = require('prop-types'),
-    Link = require('react-router-dom').Link;
-
-function PlayerPreview(props) {
-    return(
-        <div>
-            <div className="column">
-                <img
-                    className="avatar"
-                    src={props.avatar}
-                    alt={'Avatar for ' + props.username}
-                />
-                <h2 className="username">@{props.username}</h2>
-            </div>
-            <button
-                className="reset"
-                onClick={props.onReset.bind(null, props.id)}
-            >
-                Reset
-            </button>
-        </div>
-    )
-}
-PlayerPreview.propTypes = {
-    avatar: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-    onReset: PropTypes.func.isRequired
-};
+    Link = require('react-router-dom').Link,
+    PlayerPreview = require('./PlayerPreview');
 
 class PlayerInput extends React.Component {
     constructor(props) {
@@ -139,9 +113,14 @@ class Battle extends React.Component {
                         <PlayerPreview
                             avatar={playerOneImage}
                             username={playerOneName}
-                            id="playerOne"
-                            onReset={this.handleReset}
-                        />
+                        >
+                            <button
+                                className="reset"
+                                onClick={this.handleReset.bind(null, 'playerOne')}
+                            >
+                                Reset
+                            </button>
+                        </PlayerPreview>
                     }
 
                     {!playerTwoName &&
@@ -154,9 +133,14 @@ class Battle extends React.Component {
                     <PlayerPreview
                         avatar={playerTwoImage}
                         username={playerTwoName}
-                        id="playerTwo"
-                        onReset={this.handleReset}
-                    />
+                    >
+                        <button
+                            className="reset"
+                            onClick={this.handleReset.bind(null, 'playerTwo')}
+                        >
+                            Reset
+                        </button>
+                    </PlayerPreview>
                     }
                 </div>
                 {playerOneImage && playerTwoImage &&
